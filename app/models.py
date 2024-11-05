66328181c21180b67f618203162de3fdda6ebd68
gabrielfilippe <gabriel.filippe@alunos.ifsuldeminas.edu.br>
2024-11-05 16:46:23 -0300

    dando erro

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class UF(models.Model):
+    sigla=models.CharField(max_length=2)
+    
+    class Meta: 
+        verbose_name = 'Unidade Federativa'
+        verbose_name_plural = 'Unidades Federais'
+        
+    def __str__(self):
+        return self.sigla
