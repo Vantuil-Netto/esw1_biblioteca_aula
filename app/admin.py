3292356e02364fa4f9080fa5c139a492e5f07049
gabrielfilippe <gabriel.filippe@alunos.ifsuldeminas.edu.br>
2024-11-19 14:40:24 -0300

    adicionando InLines

--- app/admin.py
+++ app/admin.py
@@ -13,22 +13,43 @@ class UFAdmin(admin.ModelAdmin):
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('nome', 'cidade')
     search_fields = ('nome', 'cidade__nome')
+    
+class LivrosInline(admin.TabularInline):
+    model = Livros
+    extra = 1
 
 class GeneroAdmin(admin.ModelAdmin):
     list_display = ('nome',)
     search_fields = ('nome',)
+    inlines = [LivrosInline]
+    
+class LivrosInline(admin.TabularInline):
+    model = Livros
+    extra = 1
 
 class EditoraAdmin(admin.ModelAdmin):
     list_display = ('nome', 'cidade')
     search_fields = ('nome', 'cidade__nome')
+    inlines = [LivrosInline]
+    
+class LivrosInline(admin.TabularInline):
+    model = Livros
+    extra = 1
 
 class AutorAdmin(admin.ModelAdmin):
     list_display = ('nome', 'cidade')
     search_fields = ('nome', 'cidade__nome')
+    inlines = [LivrosInline]
+    
+class LivrosInline(admin.TabularInline):
+    model = Livros
+    extra = 1
 
 class EmprestimoAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'data_emprestimo', 'data_devolucao')
     search_fields = ('usuario__nome',)
+    inlines = [LivrosInline]
+    
 
 admin.site.register(UF, UFAdmin)
 admin.site.register(Cidade)
